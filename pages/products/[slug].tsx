c9250a6cbda6c8ea17f3579ed5cf7e5158c3c36d
Allek97 <ilyas.jsk@hotmail.ca>
2022-02-12 20:26:46 -0500

    Statically generated dynamic routes for each product

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,6 +1,11 @@
 import { getConfig } from "@framework/api/config";
 import getAllProductsPaths from "@framework/product/get-all-products-paths";
-import { GetStaticPaths, GetStaticPropsContext } from "next";
+import getProduct from "@framework/product/get-product";
+import {
+    GetStaticPaths,
+    GetStaticPropsContext,
+    InferGetStaticPropsType,
+} from "next";
 
 export const getStaticPaths: GetStaticPaths = async () => {
     const config = getConfig();
@@ -16,10 +21,26 @@ export const getStaticPaths: GetStaticPaths = async () => {
     };
 };
 
-export const getStaticProps = ({
+export const getStaticProps = async ({
     params,
 }: GetStaticPropsContext<{
     slug: string;
 }>) => {
     const config = getConfig();
+    const product = await getProduct({
+        config,
+        variables: { slug: params?.slug },
+    });
+
+    return {
+        props: {
+            product,
+        },
+    };
 };
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function ProductSlug({ product }: Props) {
+    return <div>{JSON.stringify(product, null, 2)}</div>;
+}

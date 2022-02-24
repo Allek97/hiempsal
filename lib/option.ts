f6812de99cd337b4d9e4eb8a731189ee6d7e8e78
Allek97 <ilyas.jsk@hotmail.ca>
2022-02-23 22:05:58 -0500

    Working with cart

---
+++ lib/option.ts
@@ -0,0 +1,10 @@
+type Variable = {
+    [key: string]: string;
+};
+
+export const color: Variable = {
+    "#FFFFFF": "White Smoke",
+    "#000000": "Roseman Black",
+    "#00eaff": "Electric blue",
+    "#asdfasd": "Peach Puff",
+};

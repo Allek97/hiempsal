274eba9f43da90679e19d279e4fa49e554d662d4
Allek97 <ilyas.jsk@hotmail.ca>
2022-03-04 15:41:24 -0500

    Implementing add to cart component

--- components/product/ProductView/ProductView.styled.tsx
+++ components/product/ProductView/ProductView.styled.tsx
@@ -2,11 +2,12 @@ import styled from "@emotion/styled";
 import tw from "twin.macro";
 
 export const Root = styled.div`
-    ${tw`relative max-w-7xl w-full min-height[auto]`}
+    ${tw`relative w-full min-height[auto]
+    lg:pr-11`}
 `;
 
 export const SliderContainer = styled.div`
-    ${tw`flex items-center justify-center height[82vh] overflow-x-hidden bg-accents-1`}
+    ${tw`flex items-center justify-center max-w-7xl  height[82vh] overflow-x-hidden bg-accents-1`}
     max-height: 775px;
 `;
 

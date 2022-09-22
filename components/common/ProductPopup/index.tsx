261344a6f9758e713c59bd19fb8d1584a70a50fc
Allek97 <ilyas.jsk@hotmail.ca>
2022-09-21 21:05:37 -0400

    Fixing case inconsistencies for file names so it doesn't fail building with netlify

--- components/common/ProductPopup/index.tsx
+++ components/common/ProductPopup/index.tsx
@@ -1,4 +1,1 @@
 export { ProductPopup } from "./productPopup";
-export { CartButton } from "./cartButton";
-export { ProductSelected } from "./productSelected";
-export { ProductCart } from "./productCart";
